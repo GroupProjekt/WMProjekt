817f2e262a1a3ae8b5c31b78be39e9496a533a11
T110P09A <T110P09A@schulung-provadis.de>
2025-11-21 12:42:50 +0100

    init branch

---
+++ WMProjekt/Tunierbaum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMTurnierbaum
+{
+    internal class Tunierbaum
+    {
+        Mannschaft 16tel1 = mannschaft.FirstOrDefault(m => m.Gruppenplatzierung == "A2");
+
+
+    }
+}
