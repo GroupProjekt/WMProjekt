81db0f0560b160061263665692b4bb533a311f63
T110P07A <T110P07A@schulung-provadis.de>
2025-11-21 12:34:47 +0100

    test2

--- WMProjekt/Spiel.cs
+++ WMProjekt/Spiel.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WMProjekt
+﻿public class Spiel
 {
-    internal class Spiel
+    private Mannschaft m1;
+    private Mannschaft m2;
+    private bool gruppenphase;
+    private int toreMannschaft1 = toreBerechnen(m1);
+    // test
+    private int toreMannschaft2 = toreBerechnen(m2);
+    public Spiel(Mannschaft m1, Mannschaft m2, bool gruppenphase)
     {
+        this.m1 = m1;
+        this.m2 = m2;
+        this.gruppenphase = gruppenphase;
+    }
+
+
+
+    private int toreBerechnen(Mannschaft mannschaft)
+    {
+        Random rnd = new Random();
+        int tore = rnd.Next(0, mannschaft.getMaxTore());
+        return tore;
     }
 }
